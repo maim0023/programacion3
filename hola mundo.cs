75799628b2e30221e63264dff382467aff94e2f4
maim0023 <maim0023@outlook.com>
2013-02-09 00:04:11 -0800

    Create hola mundo.cs
    primer archivo

---
+++ hola mundo.cs
@@ -0,0 +1,21 @@
+using System;
+
+espacio de nombres HelloNameSpace
+
+{
+
+      pública HelloWorld clase
+
+      {
+
+            static void main (String [] args)
+
+            {
+
+                  Console.WriteLine ("Hola Mundo!");
+
+            }
+
+      }
+
+}
